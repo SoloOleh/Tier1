85288c606f0a6ebfb8442d4534b732685ca9e1f9
Oleh Solomko <solo.oleg1987@gmail.com>
2024-02-04 17:26:58 +0200

    first three

---
+++ Модуль 1/2.py
@@ -0,0 +1,4 @@
+rate = 1.68
+value = 100
+payment = rate * value
+print (payment)
---
+++ Модуль 1/3.py
@@ -0,0 +1,5 @@
+rate = 1.68
+value_day = 100
+value_night = 60
+payment = (rate * value_day) + (rate/2 * value_night)
+print (payment)
