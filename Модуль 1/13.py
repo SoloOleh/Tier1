9f3febd06a06b81dc57e33fa32b7f8a2096503c2
Oleh Solomko <solo.oleg1987@gmail.com>
2024-02-05 18:11:40 +0200

    05.02/18:11

--- Модуль 1/13.py
+++ Модуль 1/13.py
@@ -4,9 +4,11 @@ area = length * width
 show = f"With width {width} and length {length} of the room, its area is equal to {area}"
 print (show)
 # чомусь в автоперевірці працює тільки варіант нижче
+"""
 length = "2.75"
 width = "1.75"
 length_float = float(length)
 width_float = float(width)
 area = length_float * width_float
 show = f"With width {width} and length {length} of the room, its area is equal to {area}"
+"""
